275cd8ec3d5053606072759b28f1bd1e87f0e72a
shin <jscv9636@naver.com>
2024-04-17 11:14:31 +0900

    두번째

--- src/Ex01.java
+++ src/Ex01.java
@@ -2,8 +2,44 @@
 public class Ex01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	System.out.println("나의 첫 커밋!!");	
+	System.out.println("두번째 커밋!");
+	
+	//경로변경 명령 : cd
+	// cd 바꿀경로     //저장소 확인 ex) Desktop > java > GitTest
+	// 접근 가능 경로 확인
+	// ls		
+	
+	
+	//========git 명령어 ===
+	// 1. git init(현재경로를 로컬레포지토리화 시키겠습니다)
+	// (=github에서 접근 가능하게끔 허용하겠니습니다)
+	
+	
+	// 2. 사용자 정보 등록 
+	// git config --global user.name"이름"
+	// git config --global user.email"이메일" 
+	
+	
+	// 사용자 정보 확인 
+	// git config --global --list
+	
+	// 3. 원격 저장서 등록 git remote add origin 원격저장주소 
+	// 3-1 .원격 저장소 확인 명령 git remote -v
+	
+	
+	// ----프로젝트 초기 명령어 ---
+	// 4. 커밋 
+	
+	
+	// 4-1. 커밋 활성화 git add . 
+    // 4-2 커밋 진행 git commit -m "커밋메세지"
+	// 5. 푸쉬 명령 git push origin master
+	
+		
+		
+		
 	}
 
 }
